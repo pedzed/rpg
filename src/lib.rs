2fbb639195d66f2b9d0ce91ca6f600d2e0da1dcf
pedzed <git@pedzed.com>
2020-10-01 06:20:35 +0200

    Moved run_crc to lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,1 +1,9 @@
 pub mod crc;
+
+use crc::crc24;
+
+pub fn run_crc(input: &str) {
+    let output = crc24::crc_octets(input.as_bytes());
+    println!("CRC-24 called.");
+    println!("Input:  `{}`\noutput: `{}`", input, output);
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,4 @@
 use std::env;
-use rpg::crc::crc24;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
@@ -7,15 +6,9 @@ fn main() {
     for (i, arg) in args.iter().enumerate() {
         if arg == "--crc" {
             match env::args().nth(i + 1) {
-                Some(input) => run_crc(&input),
+                Some(input) => rpg::run_crc(&input),
                 None => panic!("No input argument given for CRC."),
             }
         }
     }
-}
-
-fn run_crc(input: &str) {
-    let output = crc24::crc_octets(input.as_bytes());
-    println!("CRC-24 called.");
-    println!("Input:  `{}`\noutput: `{}`", input, output);
 }
