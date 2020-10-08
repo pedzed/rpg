3fb44bc84cd0ee275daecb821f535222b3519dd1
pedzed <git@pedzed.com>
2020-10-08 10:57:40 +0200

    Added data and checksum to ASCII Armor

--- src/radix64/armor.rs
+++ src/radix64/armor.rs
@@ -8,7 +8,11 @@
 // =njUN
 // -----END PGP MESSAGE-----
 
+use std::fs;
 use std::collections::HashMap;
+
+use super::crc24::Crc24;
+use super::coding::Radix64;
 use super::coding::LINE_ENDING;
 
 // https://tools.ietf.org/html/rfc4880#section-6.2
@@ -16,7 +20,7 @@ pub struct Armor {
     data_type: ArmorDataType,
     header_line: String,
     headers: HashMap<ArmorHeaderKey, Vec<String>>,
-    data: Vec<String>,
+    data: String,
     checksum: String,
     tail_line: String,
 }
@@ -154,7 +158,7 @@ impl Armor {
             data_type,
             header_line,
             headers: HashMap::new(),
-            data: vec![],
+            data: String::new(),
             checksum: String::new(),
             tail_line,
         }
@@ -181,8 +185,11 @@ impl Armor {
 
         armor.push_str(LINE_ENDING);
 
-        // TODO: Add Radix-64 encoding
-        // TODO: Add CRC-24 checksum
+        armor.push_str(&self.data);
+        armor.push_str(LINE_ENDING);
+
+        armor.push_str(&self.checksum);
+        armor.push_str(LINE_ENDING);
 
         armor.push_str(&format!("-----{}-----", self.tail_line));
 
@@ -250,21 +257,31 @@ mod tests {
     }
 
     #[test]
-    fn armorize_with_version_header() {
+    fn can_set_data() {
         let mut armor = Armor::new(ArmorDataType::PgpMessage);
-        armor.add_header(ArmorHeaderKey::Version, "OpenPrivacy 0.99");
 
-        assert_eq!(armor.armorize(), "\
-            -----BEGIN PGP MESSAGE-----\r\n\
-            Version: OpenPrivacy 0.99\r\n\
-            \r\n\
-            -----END PGP MESSAGE-----\
-        ");
+        let radix64 = Radix64::encode(b"Hello".to_vec());
+        armor.data = radix64.encoded;
+
+        let expected = "SGVsbG8=";
+        assert_eq!(armor.data, expected);
     }
 
     #[test]
-    fn armorize_with_multiple_comment_headers() {
+    fn can_set_checksum() {
         let mut armor = Armor::new(ArmorDataType::PgpMessage);
+
+        let crc = Crc24::new(b"Hello");
+        armor.checksum = crc.radix64_checksum();
+
+        let expected = "=EHJM";
+        assert_eq!(armor.checksum, expected);
+    }
+
+    #[test]
+    fn armorize_with_real_binary_data() {
+        let mut armor = Armor::new(ArmorDataType::PgpMessage);
+        armor.add_header(ArmorHeaderKey::Version, "OpenPrivacy 0.99");
         armor.add_header(ArmorHeaderKey::Comment, "Note that some transport methods are sensitive to line length.  While");
         armor.add_header(ArmorHeaderKey::Comment, "there is a limit of 76 characters for the Radix-64 data (Section");
         armor.add_header(ArmorHeaderKey::Comment, "6.3), there is no limit to the length of Armor Headers.  Care should");
@@ -273,17 +290,63 @@ mod tests {
         armor.add_header(ArmorHeaderKey::Comment, "multiple times with different values for each so that no one line is");
         armor.add_header(ArmorHeaderKey::Comment, "overly long.");
 
-        assert_eq!(armor.armorize(), "\
-            -----BEGIN PGP MESSAGE-----\r\n\
-            Comment: Note that some transport methods are sensitive to line length.  While\r\n\
-            Comment: there is a limit of 76 characters for the Radix-64 data (Section\r\n\
-            Comment: 6.3), there is no limit to the length of Armor Headers.  Care should\r\n\
-            Comment: be taken that the Armor Headers are short enough to survive\r\n\
-            Comment: transport.  One way to do this is to repeat an Armor Header key\r\n\
-            Comment: multiple times with different values for each so that no one line is\r\n\
-            Comment: overly long.\r\n\
-            \r\n\
-            -----END PGP MESSAGE-----\
-        ");
+        let data_bytes = fs::read("tests/resources/gnupg-icon.png").unwrap();
+
+        let crc = Crc24::new(data_bytes.as_slice());
+        armor.checksum = crc.radix64_checksum();
+
+        armor.data = Radix64::encode(data_bytes).encoded;
+
+        let armorized = armor.armorize();
+        let mut lines: Vec<&str> = armorized.lines().map(From::from).collect();
+
+        let mut expected: Vec<&str> = vec![
+            "-----BEGIN PGP MESSAGE-----",
+            "Version: OpenPrivacy 0.99",
+            "Comment: Note that some transport methods are sensitive to line length.  While",
+            "Comment: there is a limit of 76 characters for the Radix-64 data (Section",
+            "Comment: 6.3), there is no limit to the length of Armor Headers.  Care should",
+            "Comment: be taken that the Armor Headers are short enough to survive",
+            "Comment: transport.  One way to do this is to repeat an Armor Header key",
+            "Comment: multiple times with different values for each so that no one line is",
+            "Comment: overly long.",
+            "",
+            "iVBORw0KGgoAAAANSUhEUgAAAIAAAACACAMAAAD04JH5AAAAM1BMVEUAk90NmN48",
+            "reaV0/F2xe0Ane284/f///8AjdsAmOb2+/7W7vro9fwno+Ku3fVStehcuumhrI4O",
+            "AAAFcklEQVR42u2b2ZLcIAxFAWEas///14bF7gav4G2qUtZDMnnI3IMQSAaBPidM",
+            "KClZMKmEOPg70MH/pxg4ojFCqPeGENaWghQPAUgguu+7LigPFv+JNDXidgAFBAex",
+            "Jeu7XlN5K4CkXh1tWN8hx24DUF5+HDsP9vuJ/xg6ZOUtAAL0IO/1MHEUTFwDBoD6",
+            "gMwgOgziegBGkrzXIdSomXcMJWhk6DsiLwYQ3vtJPgsz6UdPqXM0esNvCj5Av04w",
+            "lwKoNHwvD2pci87vAzwzjImlFn8XJlwIwHQa/ji3ficI2oiXS6CIxb6jlwEYnIbv",
+            "0uiZw5yjfashqAKAuOX54afRW1SjXkmA6vW1TMFYLV9FUAFgkj5RKRga5EMgwGkA",
+            "hjN9QG36qMfsJIDSyf9BX7hG+UCg1SkAQboYfzLuBe36PgzcKQCa1r9Z0+9TPXI8",
+            "DNBeAEZ9GueCz5J/j7U3HH7ccAFWhwFUCkAtwlyU+l2PHTDli0FfGfo8hNcZNtfi",
+            "NoD7ToCwvJS3k3woqV5HkAcBWJoAG2KBFxNP2FKthldqpY4cA0grIPIDLyZ1JayU",
+            "WwnInh0CgG50gMz3n61aA5YJNlywASB0+mWsDMDOit19s8EFaNcBpAyAbf01gs62",
+            "AwwO4PBhhTNFVe6aGpbNAGkPQljlE7C3s/88V7sXrAPYMQTzFdDXVJqkW8pJohEg",
+            "bYJ+E1K4ZiqLPQktTMIaOtpxJFZZBPa4rtinXX1SRDszQFi2Bexk1p8LFlbCWvSg",
+            "nTVAixqk9qvTddVzgHbmkWQR0JPazyjTV6+DNQCGcJTGjWV+EcGlB5Z3ELSRCDHG",
+            "5RigFuBDFgCWIxhtZmL0ArwAL8AL8AK8AC/AC/ACvAAvwP8FoE4DmDMAHFV6AKg3",
+            "cOG7aGqW1pqe/lf/lWkqP81IF2z5rKurtdnwtak+oiE7h98I7/8xk1+/SD0A0Goc",
+            "g2o5qr0YgGOq2u4LLgXw8rL1zug6AL4vfyMAD7fc+6ea9wCEK2RtV9pJ5L0AsZdC",
+            "E8rW1h3QuwD4qG3YhudpfznA0EESegfAyO1ZF45fBcDHzhWsB+WKphFJ+OysrxVg",
+            "HK/XdRQqhcfjS8zRCYAkrFPzzIGmLUFj08NBgCG8WsY7Hf5w8XoEwO9oFlJ4HdWX",
+            "bjx0bwfgiID6nDIBmK8dOO8BcG7Z56R572eXHm0A65VMtTHyk/eT2eQBjqg4LY8y",
+            "eQ2SNABwfdb7ZiIvPqZhCrg9F3sKCufHulDoagDO4ZS8pHgmHy6Ba2OAozP6wthC",
+            "fqwLWfUy5JidGXy+7rIGvNCGUgcwtG0dUodfW2Nqfvx9E7jaZHRYX0Hu+uj7zJNQ",
+            "m4yGtq3WefdjLyY+ZJD8F6VL8BqAA/qhofSrHrsrZ22nBlUC6EZ9IVM362ixy9vM",
+            "5nDQrwCgjeMXLHzQO2edN1grVqA+HZ/d/ZeL4YZ0fIOprA3rLwCKLsznAVTZhPo4",
+            "gJl0AT4MkJdDfwDAFjqQHwRgiw3QTwEoIMsdwI8ACFZUBA8DePW8IngYQBmnp+q+",
+            "JMJPAMSsiPlM3ZdEcLcHQl+v1TPxdFLGfC1+01YcnpoZoJaklx4LJ2XprIpcvBUr",
+            "A84SbxpjxOfSw0mZHZ+elR3xV3hASAbJ44X8ryIi+bs3elcuUMH9LvohmSbEUgpl",
+            "RTTpyL6jIhJCBRNi6cmhnHXkP1uSZScjf5ENJVnYEJ/zgKKYH9qKxZ3yTwEwio9n",
+            "w9MAq6XAIwCT7+NnARSjZPcV3C7AsRfMMRdrVPEG72oPhCc28Qn0QlK61gPqu+H6",
+            "v6SU8alrenxdq33cA6HM8zlHDxYuwzninDcpH/eAAM1LO3Ot1g4Qzrr766w5GSl2",
+            "sU0Ob/4BrGxXIweWt2UAAAAASUVORK5CYII=",
+            "=/u+x",
+            "-----END PGP MESSAGE-----",
+        ];
+
+        assert_eq!(lines.sort(), expected.sort());
     }
 }
