f28f1eed9d23de4cc7ad62c9603255a39b5487f7
pedzed <git@pedzed.com>
2020-10-13 10:07:05 +0200

    Added Radix-64 decoding

--- src/radix64/coding.rs
+++ src/radix64/coding.rs
@@ -112,8 +112,132 @@ impl Radix64 {
     }
 
     pub fn decode(encoded: &str) -> Self {
-        unimplemented!();
+        let encoded_without_padding = encoded_without_padding(encoded);
+        let encoded_normalized = encoded_without_padding.replace("\r\n", "");
+
+        let encoded_chunks = encoded_normalized.as_bytes().chunks(BLOCKS_PER_SEXTET).collect::<Vec<_>>();
+        let (last_chunk, main_chunks) = encoded_chunks.split_last().unwrap();
+
+        let mut unencoded;
+
+        if last_chunk.len() == BLOCKS_PER_SEXTET {
+            unencoded = decode_full_chunks(&encoded_chunks);
+        } else {
+            let mut unencoded_partial = decode_partial_chunk(last_chunk);
+
+            unencoded = decode_full_chunks(main_chunks);
+            unencoded.append(&mut unencoded_partial);
+        }
+
+        Self {
+            unencoded,
+            encoded: String::from(encoded),
+        }
     }
+}
+
+fn encoded_without_padding(encoded: &str) -> &str {
+    let padding_count = encoded.as_bytes()
+        .iter()
+        .rev()
+        .take_while(|&b| *b == b'=')
+        .count()
+    ;
+
+    match padding_count {
+        0 => &encoded[0..encoded.len()-0],
+        1 => &encoded[0..encoded.len()-1],
+        2 => &encoded[0..encoded.len()-2],
+        _ => unreachable!("Padding count not possible."),
+    }
+}
+
+fn decode_full_chunks(full_chunks: &[&[u8]]) -> Vec<u8> {
+    let mut unencoded: Vec<u8> = vec![];
+
+    for encoded_chunk in full_chunks {
+        let mut decoded_sextets: [u8; BLOCKS_PER_SEXTET] = [INVALID_VALUE; BLOCKS_PER_SEXTET];
+
+        for (i, encoded_char) in encoded_chunk.iter().enumerate() {
+            decoded_sextets[i] = tables::STD_DECODE[*encoded_char as usize];
+
+            if decoded_sextets[i] == INVALID_VALUE {
+                // TODO: Replace with catchable error
+                panic!("Unexpected value to decode.");
+            }
+        }
+
+        let decoded_chunk: u32 =
+            ((decoded_sextets[0] as u32) << 18) |
+            ((decoded_sextets[1] as u32) << 12) |
+            ((decoded_sextets[2] as u32) << 6) |
+            ((decoded_sextets[3] as u32) << 0)
+        ;
+
+        let decoded_octet_chunk: [u8; BLOCKS_PER_OCTET] = [
+            ((decoded_chunk & 0b11111111_00000000_00000000) >> 16) as u8,
+            ((decoded_chunk & 0b00000000_11111111_00000000) >> 8) as u8,
+            ((decoded_chunk & 0b00000000_00000000_11111111) >> 0) as u8,
+        ];
+
+        for &octet in decoded_octet_chunk.iter() {
+            if octet == INVALID_VALUE {
+                // TODO: Replace with catchable error
+                panic!("Unexpected value to decode.");
+            }
+
+            unencoded.push(octet);
+        }
+    }
+
+    unencoded
+}
+
+fn decode_partial_chunk(partial_chunk: &[u8]) -> Vec<u8> {
+    let mut unencoded: Vec<u8> = vec![];
+
+    match partial_chunk.len() {
+        2 => {
+            let mut decoded_sextets: [u8; 2] = [INVALID_VALUE; 2];
+
+            for (i, encoded_char) in partial_chunk.iter().enumerate() {
+                decoded_sextets[i] = tables::STD_DECODE[*encoded_char as usize];
+
+                if decoded_sextets[i] == INVALID_VALUE {
+                    // TODO: Replace with catchable error
+                    panic!("Unexpected value to decode.");
+                }
+            }
+
+            let decoded_octet: u8 = (decoded_sextets[0] << 2) | (decoded_sextets[1] >> 4);
+
+            unencoded.push(decoded_octet);
+        },
+        3 => {
+            let mut decoded_sextets: [u8; 3] = [INVALID_VALUE; 3];
+
+            for (i, encoded_char) in partial_chunk.iter().enumerate() {
+                decoded_sextets[i] = tables::STD_DECODE[*encoded_char as usize];
+
+                if decoded_sextets[i] == INVALID_VALUE {
+                    // TODO: Replace with catchable error
+                    panic!("Unexpected value to decode.");
+                }
+            }
+
+            let decoded_octet_chunk: [u8; 2] = [
+                ((decoded_sextets[0] << 2) | (decoded_sextets[1] >> 4)) as u8,
+                ((decoded_sextets[1] << 4) | (decoded_sextets[2] >> 2)) as u8,
+            ];
+
+            for octet in decoded_octet_chunk.iter() {
+                unencoded.push(*octet);
+            }
+        },
+        x => unreachable!("Partial chunk size of {} is not possible.", x),
+    }
+
+    unencoded
 }
 
 #[cfg(test)]
@@ -171,7 +295,15 @@ mod tests {
 
     #[test]
     fn can_encode_longer_text() {
-        let unencoded = b"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.".to_vec();
+        let unencoded = b"\
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do \
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut \
+            enim ad minim veniam, quis nostrud exercitation ullamco laboris \
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in \
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat \
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident, \
+            sunt in culpa qui officia deserunt mollit anim id est laborum.\
+        ".to_vec();
 
         let expected = "\
             TG9yZW0gaXBzdW0gZG9sb3Igc2l0IGFtZXQsIGNvbnNlY3RldHVyIGFkaXBpc2Np\r\n\
@@ -191,73 +323,103 @@ mod tests {
         assert_eq!(radix64.encoded, expected);
     }
 
-    // #[test]
-    // fn can_decode_1_char() {
-    //     let radix64 = Radix64::decode("SA==");
-    //     assert_eq!(radix64.encoded, String::from("SA=="));
-    //     assert_eq!(radix64.unencoded, b"H".to_vec());
-    // }
+    #[test]
+    fn can_get_encoding_without_padding_0() {
+        assert_eq!(encoded_without_padding("SGVs"), "SGVs");
+    }
 
-    // #[test]
-    // fn can_decode_2_chars() {
-    //     let radix64 = Radix64::decode("SGU=");
-    //     assert_eq!(radix64.encoded, String::from("SGU="));
-    //     assert_eq!(radix64.unencoded, b"He".to_vec());
-    // }
+    #[test]
+    fn can_get_encoding_without_padding_1() {
+        assert_eq!(encoded_without_padding("SGU="), "SGU");
+    }
 
-    // #[test]
-    // fn can_decode_3_chars() {
-    //     let radix64 = Radix64::decode("SGVs");
-    //     assert_eq!(radix64.encoded, String::from("SGVs"));
-    //     assert_eq!(radix64.unencoded, b"Hel".to_vec());
-    // }
+    #[test]
+    fn can_get_encoding_without_padding_2() {
+        assert_eq!(encoded_without_padding("SA=="), "SA");
+    }
 
-    // #[test]
-    // fn can_decode_4_chars() {
-    //     let radix64 = Radix64::decode("SGVsbA==");
-    //     assert_eq!(radix64.encoded, String::from("SGVsbA=="));
-    //     assert_eq!(radix64.unencoded, b"Hell".to_vec());
-    // }
+    #[test]
+    fn can_decode_2_chars() {
+        let radix64 = Radix64::decode("SA==");
+        assert_eq!(radix64.encoded, String::from("SA=="));
+        assert_eq!(radix64.unencoded, b"H".to_vec());
+    }
 
-    // #[test]
-    // fn can_decode_5_chars() {
-    //     let radix64 = Radix64::decode("SGVsbG8=");
-    //     assert_eq!(radix64.encoded, String::from("SGVsbG8="));
-    //     assert_eq!(radix64.unencoded, b"Hello".to_vec());
-    // }
+    #[test]
+    fn can_decode_3_chars() {
+        let radix64 = Radix64::decode("SGU=");
+        assert_eq!(radix64.encoded, String::from("SGU="));
+        assert_eq!(radix64.unencoded, b"He".to_vec());
+    }
 
-    // #[test]
-    // fn can_decode_6_chars() {
-    //     let radix64 = Radix64::decode("SGVsbG8h");
-    //     assert_eq!(radix64.encoded, String::from("SGVsbG8h!"));
-    //     assert_eq!(radix64.unencoded, b"Hello!".to_vec());
-    // }
+    #[test]
+    fn can_decode_4_chars() {
+        let radix64 = Radix64::decode("SGVs");
+        assert_eq!(radix64.encoded, String::from("SGVs"));
+        assert_eq!(radix64.unencoded, b"Hel".to_vec());
+    }
 
-    // #[test]
-    // fn can_decode_12_chars() {
-    //     let radix64 = Radix64::decode("SGVsbG8gV29ybGQh");
-    //     assert_eq!(radix64.encoded, String::from("SGVsbG8gV29ybGQh"));
-    //     assert_eq!(radix64.unencoded, b"Hello World!".to_vec());
-    // }
+    #[test]
+    fn can_decode_6_chars() {
+        let radix64 = Radix64::decode("SGVsbA==");
+        assert_eq!(radix64.encoded, String::from("SGVsbA=="));
+        assert_eq!(radix64.unencoded, b"Hell".to_vec());
+    }
 
-    // #[test]
-    // fn can_decode_longer_text() {
-    //     let encoded = "\
-    //         TG9yZW0gaXBzdW0gZG9sb3Igc2l0IGFtZXQsIGNvbnNlY3RldHVyIGFkaXBpc2Np\r\n\
-    //         bmcgZWxpdCwgc2VkIGRvIGVpdXNtb2QgdGVtcG9yIGluY2lkaWR1bnQgdXQgbGFi\r\n\
-    //         b3JlIGV0IGRvbG9yZSBtYWduYSBhbGlxdWEuIFV0IGVuaW0gYWQgbWluaW0gdmVu\r\n\
-    //         aWFtLCBxdWlzIG5vc3RydWQgZXhlcmNpdGF0aW9uIHVsbGFtY28gbGFib3JpcyBu\r\n\
-    //         aXNpIHV0IGFsaXF1aXAgZXggZWEgY29tbW9kbyBjb25zZXF1YXQuIER1aXMgYXV0\r\n\
-    //         ZSBpcnVyZSBkb2xvciBpbiByZXByZWhlbmRlcml0IGluIHZvbHVwdGF0ZSB2ZWxp\r\n\
-    //         dCBlc3NlIGNpbGx1bSBkb2xvcmUgZXUgZnVnaWF0IG51bGxhIHBhcmlhdHVyLiBF\r\n\
-    //         eGNlcHRldXIgc2ludCBvY2NhZWNhdCBjdXBpZGF0YXQgbm9uIHByb2lkZW50LCBz\r\n\
-    //         dW50IGluIGN1bHBhIHF1aSBvZmZpY2lhIGRlc2VydW50IG1vbGxpdCBhbmltIGlk\r\n\
-    //         IGVzdCBsYWJvcnVtLg=="
-    //     ;
-    //     let expected = "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.";
+    #[test]
+    fn can_decode_7_chars() {
+        let radix64 = Radix64::decode("SGVsbG8=");
+        assert_eq!(radix64.encoded, String::from("SGVsbG8="));
+        assert_eq!(radix64.unencoded, b"Hello".to_vec());
+    }
 
-    //     let radix64 = Radix64::decode(encoded);
+    #[test]
+    fn can_decode_8_chars() {
+        let radix64 = Radix64::decode("SGVsbG8h");
+        assert_eq!(radix64.encoded, String::from("SGVsbG8h"));
+        assert_eq!(radix64.unencoded, b"Hello!".to_vec());
+    }
 
-    //     assert_eq!(radix64.encoded, expected);
-    // }
+    #[test]
+    fn can_decode_16_chars() {
+        let radix64 = Radix64::decode("SGVsbG8gV29ybGQh");
+        assert_eq!(radix64.encoded, String::from("SGVsbG8gV29ybGQh"));
+        assert_eq!(radix64.unencoded, b"Hello World!".to_vec());
+    }
+
+    #[test]
+    fn can_decode_longer_text() {
+        let encoded = "\
+            TG9yZW0gaXBzdW0gZG9sb3Igc2l0IGFtZXQsIGNvbnNlY3RldHVyIGFkaXBpc2Np\r\n\
+            bmcgZWxpdCwgc2VkIGRvIGVpdXNtb2QgdGVtcG9yIGluY2lkaWR1bnQgdXQgbGFi\r\n\
+            b3JlIGV0IGRvbG9yZSBtYWduYSBhbGlxdWEuIFV0IGVuaW0gYWQgbWluaW0gdmVu\r\n\
+            aWFtLCBxdWlzIG5vc3RydWQgZXhlcmNpdGF0aW9uIHVsbGFtY28gbGFib3JpcyBu\r\n\
+            aXNpIHV0IGFsaXF1aXAgZXggZWEgY29tbW9kbyBjb25zZXF1YXQuIER1aXMgYXV0\r\n\
+            ZSBpcnVyZSBkb2xvciBpbiByZXByZWhlbmRlcml0IGluIHZvbHVwdGF0ZSB2ZWxp\r\n\
+            dCBlc3NlIGNpbGx1bSBkb2xvcmUgZXUgZnVnaWF0IG51bGxhIHBhcmlhdHVyLiBF\r\n\
+            eGNlcHRldXIgc2ludCBvY2NhZWNhdCBjdXBpZGF0YXQgbm9uIHByb2lkZW50LCBz\r\n\
+            dW50IGluIGN1bHBhIHF1aSBvZmZpY2lhIGRlc2VydW50IG1vbGxpdCBhbmltIGlk\r\n\
+            IGVzdCBsYWJvcnVtLg==\
+        ";
+        let expected = b"\
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do \
+            eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut \
+            enim ad minim veniam, quis nostrud exercitation ullamco laboris \
+            nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in \
+            reprehenderit in voluptate velit esse cillum dolore eu fugiat \
+            nulla pariatur. Excepteur sint occaecat cupidatat non proident, \
+            sunt in culpa qui officia deserunt mollit anim id est laborum.\
+        ";
+
+        let radix64 = Radix64::decode(encoded);
+
+        assert_eq!(radix64.unencoded.len(), expected.len());
+        assert_eq!(radix64.unencoded[0..32], expected[0..32]);
+
+        let len = expected.len();
+        assert_eq!(
+            radix64.unencoded[len-32..len],
+            expected[len-32..len]
+        );
+    }
 }
