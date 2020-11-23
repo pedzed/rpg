85bb5e6761e79421b9350e714aa1fc83287cfd88
pedzed <git@pedzed.com>
2020-11-23 09:58:30 +0100

    Added working key schedule for AES-128

--- src/aes.rs
+++ src/aes.rs
@@ -1,6 +1,10 @@
 pub mod state;
+pub mod round_key;
+
 pub mod sbox_tables;
 pub mod sub_bytes;
 pub mod shift_rows;
 pub mod mix_columns;
 pub mod add_round_key;
+
+pub mod key_schedule;
--- src/aes/round_key.rs
+++ src/aes/round_key.rs
@@ -1,40 +1,4 @@
-use std::ops::Index;
+use super::state;
 
-pub type Element = u8;
-pub type Elements = [Element; RoundKey::CAPACITY];
-
-type Row = usize;
-type Column = usize;
-
-/// The RoundKey of an AES block
-///
-/// Stored as a one-dimensional list of bytes.
-///
-/// | 0  1  2  3|
-/// | 4  5  6  7|
-/// | 8  9 10 11|
-/// |12 13 14 15|
-#[derive(Debug, PartialEq)]
-pub struct RoundKey {
-    elements: Elements,
-}
-
-impl Index<usize> for RoundKey {
-    type Output = Element;
-
-    fn index(&self, index: usize) -> &Self::Output {
-        &self.elements[index]
-    }
-}
-
-impl RoundKey {
-    const ROWS: Row = 4;
-    const COLUMNS: Column = 4;
-    pub const CAPACITY: usize = Self::ROWS * Self::COLUMNS;
-
-    pub fn new(elements: Elements) -> Self {
-        Self {
-            elements,
-        }
-    }
-}
+pub type Element = state::Element;
+pub type RoundKey = state::State;
